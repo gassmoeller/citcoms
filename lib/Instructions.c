ccc75062c4d9552108e354a7c25520709accdefd
Rene Gassmoeller <rengas@gfz-potsdam.de>
2013-04-18 14:57:56 +0000

    Removed input for continents from Instructions.c. It is read in in Composition_related.c.

--- lib/Instructions.c
+++ lib/Instructions.c
@@ -741,8 +741,6 @@ void read_initial_settings(struct All_variables *E)
   E->composition.delta_temp = (E->composition.end_temp - E->composition.start_temp)
                                      / (float) (E->composition.ntdeps-1);
 
-  input_int("continents",&(E->composition.continents),"0,0,nomax",m);
-
 
   tracer_input(E);
 
